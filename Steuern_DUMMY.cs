d849d6a08228863b064de971053dee27063e953a
eliemengi <126130505+eliemengi@users.noreply.github.com>
2025-08-10 20:48:15 +0200

    Steuern_DUMMY.cs
    Dummy implementation of tax-related data, including applicable tax rates and loss carryforward information. Used to test tax calculation routines in isolation.

---
+++ Steuern_DUMMY.cs
@@ -0,0 +1,6 @@
+﻿// Steuern_DUMMY.cs (zu Zulassungsaufgabe 24W)
+
+namespace EasyBankingMiniGuV.Datenhaltung.Transfer
+{
+    public record Steuern(int Id, int PeriodeId, decimal ErträgeAufwendungen, decimal Verlustvortrag);
+}
