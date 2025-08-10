14ef7fba16630de3f2bac204d071898022019641
eliemengi <126130505+eliemengi@users.noreply.github.com>
2025-08-10 20:49:12 +0200

    Add files via upload

---
+++ Zinsüberschuss_DUMMY.cs
@@ -0,0 +1,6 @@
+﻿// Zinsüberschuss_DUMMY.cs (zu Zulassungsaufgabe 24W)
+
+namespace EasyBankingMiniGuV.Datenhaltung.Transfer
+{
+    public record Zinsüberschuss(int Id, int PeriodeId, decimal ZinsüberschussBrutto, decimal WertberichtigungenAbschreibungen, decimal Risikovorsorge);
+}
